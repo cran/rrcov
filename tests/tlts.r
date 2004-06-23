64dc206cf2a3e660941e4f5f3b4ce341bbae8e36
Valentin Todorov <valentin.todorov@chello.at>
2004-06-23 00:00:00 +0000

    version 0.2-1

--- tests/tlts.r
+++ tests/tlts.r
@@ -51,11 +51,84 @@ dodata <- function(nrep=1, time=FALSE, short=FALSE){
     cat("========================================================\n")
 }
 
-dorep <- function(x, y, nrep=1){ 
+dorep <- function(x, y, nrep=1, MASS=FALSE){ 
 
+    # set mcd=FALSE - we want to time only the LTS algorithm
     for(i in 1:nrep)
-        ltsReg(x, y)
+    if(MASS)
+            ltsreg(x,y)
+        else
+            ltsReg(x, y, mcd = FALSE)
 } 
+
+dogen <- function(nrep=1, eps=0.4, MASS=FALSE){
+
+    dolts <- function(x, y, nrep=1, MASS=FALSE){ 
+        gc()
+        xtime <- system.time(dorep(x, y, nrep, MASS))[1]/nrep
+        n <- as.integer(dim(x)[1])
+        p <- as.integer(dim(x)[2] + 1)
+        cat(sprintf("%6d %3d %10.2f\n", n, p, xtime))
+        xtime   
+    } 
+
+    set.seed(0)
+
+    library(MASS)
+    library(rrcov)
+#    ap <- c(2, 5, 10, 20, 30)
+#    an <- c(100, 500, 1000, 10000, 50000)
+    ap <- c(2, 3, 5, 10)
+    an <- c(100, 500, 1000, 10000, 50000)
+
+
+    tottime <- 0
+    cat("     n   p       Time\n")
+    cat("=====================\n")
+    for(i in 1:length(an)) {
+        for(j in 1:length(ap)) {
+            n <- an[i]
+            p <- ap[j]
+            if(5*p <= n){
+                a <- gendata(n, p, eps)
+                tottime <- tottime + dolts(a$x,a$y, nrep, MASS)
+            }
+        } 
+    }
+    
+    cat("=====================\n")
+    cat("Total time: ", tottime*nrep, "\n")
+}
+
+#### gendata() ####
+# Generates a data set with bad leverage points (outliers in x-space) 
+# n observations in p dimensions acording to the model:
+#   yi = Xi1+Xi2+...+ei
+# where ei - N(0,1) is the error term, Xi,j for j=1...p-1 - N(0,100) are 
+# the non-trivial explanatory variables and xip is the intercept term.
+# The outliers in the x-space are introduced by replacing eps. percent of
+# xi1 by values distributed as N(100,100).
+#
+# Defaults: eps=0
+#
+gendata <- function(n,p,eps=0){
+
+    if(eps < 0 || eps >= 0.5)
+        stop(message="eps must be in [0,0.5)")
+
+    p <- p-1
+    x <- matrix(rnorm(n*(p),0,100), c(n,p))
+    y <-rowSums(x) + 1 + rnorm(n, 0, 1)
+    
+    nbad <- as.integer(eps * n)
+    xind <- sort(sample(n,nbad))
+    xbad <- rnorm(nbad,100,100)
+    for(i in 1:nbad){
+        x[xind[i],1] <- xbad[i] 
+    }
+    list(x=x, y=y, xind=xind)
+}
+
 
 pad.right <- function(z, pads)
 {
